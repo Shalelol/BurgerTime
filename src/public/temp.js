1ed2fd1e058804cbc89113d3e3fd78b7186591e0
Shale Kuzmanovski <Shale.lol@hotmail.com>
2016-02-24 22:51:30 +1100

    Set up jade templating engine to give it a go Created some logic to count down to a certain time Burger will now slowly fill, becoming a full burger at the goal time

--- src/public/temp.js
+++ src/public/temp.js
@@ -1,3 +1,14 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
 $(function() {
   var $topLeft = $('.top-left .blocking');
   var $topRight = $('.top-right .blocking');
@@ -10,7 +21,7 @@ $(function() {
 
 	var progress = current / total;
 
-  
+
     if(progress < 0.25) {
       $topRight.css('transform', 'rotate(-' + (90 - (progress * 360)) + 'deg)');
     } else if (progress >= 0.25 && progress < 0.50) {
@@ -33,16 +44,25 @@ $(function() {
     }
   }
 
-  var value = 0;
+  var time = +getParameterByName('time');
+
+  if(!time) return;
+
+  if((new Date()).valueOf() > time) {
+    console.log((new Date()).valueOf() - time);
+    console.log((new Date()).valueOf());
+    return;
+  }
+
+  var openTime = (new Date()).valueOf();
 
   var timeout = function(){
-      calcBackground(value, 100);
-      value++;
+      var now = (new Date()).valueOf() - openTime;
+      calcBackground(now, time - openTime);
+      $('.goal-time').text(moment(time).countdown().toString());
 
-      if(value > 100)
+      if(now > time)
         window.clearInterval(timeout);
     }
   window.setInterval(timeout, 100);
-
-  window.calcBackground = calcBackground;
 });
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'jade');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Burger time');
+  res.render('index')
 });
 
 const server = app.listen(3000, () => console.log('App listening at http://localhost:3000'));
